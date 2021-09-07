8e7f5cf938bd12a6c680ab5060ea1da8246951d7
jackalotUVM <gonzalezc9999@gmail.com>
2021-09-07 08:25:12 -0400

    computerPlay indentation change

--- Script.js
+++ Script.js
@@ -1,17 +1,16 @@
-computerPlay();
 function computerPlay () {
-let compChoice = Math.floor(Math.random() * 3);
-console.log(compChoice);
-switch(compChoice)
-{
-    case 0:
-    return "rock";
-    case 1:
-    return "paper";
-    case 2:
-    return "scissors";
-    default:
-    console.log("something happened with the comp choice");
-    break;
-}
+    let compChoice = Math.floor(Math.random() * 3);
+    console.log(compChoice);
+    switch(compChoice)
+    {
+        case 0:
+        return "rock";
+        case 1:
+        return "paper";
+        case 2:
+        return "scissors";
+        default:
+        console.log("something happened with the comp choice");
+        break;
+    }
 }
